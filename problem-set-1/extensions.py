869d1aae9d1e5d01be8e0257cdc0179df87a5e01
sandypesic <sandy.pesic@gmail.com>
2025-10-01 18:33:35 -0700

    Update extensions.py

--- problem-set-1/extensions.py
+++ problem-set-1/extensions.py
@@ -1,11 +1,17 @@
 user_input = input("File name: ").strip().lower()
-if user_input[-4:] == ".gif" or user_input[-4:] == ".png":
-    print("image/" + user_input[-3:])
-elif user_input[-4:] == ".jpg" or user_input[-5:] == ".jpeg":
-    print("image/jpeg")
-elif user_input[-4:] == ".pdf" or user_input[-4:] == ".zip":
-    print("application/" + user_input[-3:])
-elif user_input[-4:] == ".txt":
-    print("text/" + user_input[-3:])
-else:
-    print("application/octet-stream")
+
+match user_input:
+    case _ if user_input.endswith(".gif"):
+        print("image/gif")
+    case _ if user_input.endswith(".png"):
+        print("image/png")
+    case _ if user_input.endswith(".jpg") | user_input.endswith(".jpeg"):
+        print("image/jpeg")
+    case _ if user_input.endswith(".pdf"):
+        print("application/pdf")
+    case _ if user_input.endswith(".zip"):
+        print("application/zip")
+    case _ if user_input.endswith(".txt"):
+        print("text/plain")
+    case _:
+        print("application/octet-stream")
